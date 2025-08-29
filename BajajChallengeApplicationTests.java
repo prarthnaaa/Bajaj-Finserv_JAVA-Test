dbb32120fdd2b5258afb058e8869a7fdae895cb1
Prarthna Puhan <105586846+prarthnaaa@users.noreply.github.com>
2025-08-29 11:18:38 +0530

    Add files via upload

---
+++ BajajChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrarthnaPuhan.bajaj_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
